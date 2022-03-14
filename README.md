6b9c489cceb3cc17d04f29961575e8eb167cac89
unic0rn9k <unic0rn9k@protonmail.com>
2022-03-14 18:50:39 +0100

    Example in readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 ## Static Fast Fourier Transform
 
 Implementation developed at [my fourier notebook](https://nbviewer.org/github/unic0rn9k/fourier-notebook/blob/master/README.ipynb).
+
+```rust
+use sfft::*;
+
+let v = fft([re(0f32), re(1.), re(2.), re(3.)]);
+
+assert_eq!(
+    v,
+    [
+        re(6f32) + im(0.),
+        re(-1.9999999) + im(2.),
+        re(-2.) + im(0.),
+        re(-2.) - im(2.),
+    ]
+)
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
 //! # Static Fast Fourier Transform
 //!
 //! Implementation developed at [my fourier notebook](https://nbviewer.org/github/unic0rn9k/fourier-notebook/blob/master/README.ipynb).
+//!
+//! ```rust
+//! use sfft::*;
+//!
+//! let v = fft([re(0f32), re(1.), re(2.), re(3.)]);
+//!
+//! assert_eq!(
+//!     v,
+//!     [
+//!         re(6f32) + im(0.),
+//!         re(-1.9999999) + im(2.),
+//!         re(-2.) + im(0.),
+//!         re(-2.) - im(2.),
+//!     ]
+//! )
+//! ```
 
-use num::*;
+pub use num::*;
 use std::f32::consts::PI;
 
 pub unsafe fn unsafe_fft<const LEN: usize>(
